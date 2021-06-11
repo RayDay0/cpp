57ca79a932a960b7710ccbd2e1198fd2ab9890be
Ray <rayday0@yandex.ru>
2021-06-11 16:26:07 +0300

    Launch Git

---
+++ SimpleCode/SimpleCode/SimpleCode.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+void FillArray(int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
+void ShowArray(const int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void push_back(int*& arr, int& size,const int value)
+{
+	int* newArray = new int[size + 1];
+	for (int i = 0; i < size; i++)
+	{
+		newArray[i] = arr[i];
+	}
+	newArray[size++] = value; 
+	delete[]arr;
+	arr = newArray;
+}
+
+void pop_back(int*& arr, int& size)
+{
+	size--;
+	int* newArray = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		newArray[i] = arr[i];
+	}
+		delete[] arr;
+		arr = newArray;
+}
+
+int main()
+{
+	srand(time(NULL));
+	int size = 5;
+	int* arr = new int[size];
+
+	FillArray(arr, size);
+	ShowArray(arr, size);
+
+	push_back(arr, size, 11);
+	ShowArray(arr, size);
+
+	pop_back(arr, size);
+	ShowArray(arr, size);
+
+	delete[]arr;
+}
+
+
---
+++ anyCode/anyCode/anyCode.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int numberTranslation_2in10(int& x) {
+	int a, b, c, d;
+	a = x / 100;
+	b = x / 10 % 10;
+	c = x  % 10;
+	return  x  = (a * 4 + b * 2 + c);
+}
+
+int numberTranslation_3in10(int& x) {
+	int a, b, c, d;
+	a = x / 100;
+	b = x / 10 % 10;
+	c = x % 10;
+	return  x = (a * 9 + b * 3 + c);
+}
+
+int numberTranslation_10in4(int x, int y) {
+	int z, a, b, c, d;
+	z = x + y;
+	a = z / 64;
+	b = z / 16 % 4;
+	c = z / 4 % 4;
+	d = z % 4;
+	return (a * 1000 + b * 100 + c * 10 + d);
+}
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	numberTranslation_2in10(x);
+	numberTranslation_3in10(y);
+	cout << x << " + " << y << " = " << x + y;
+	cout << endl;
+	cout << numberTranslation_10in4(x, y);
+}
